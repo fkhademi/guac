acb0bb29516d13a2aceb3bec6c461cb5ce132065
Frey Khademi <frey.khademi@gmail.com>
2021-02-08 18:38:39 +0100

    logging

--- guac_install.sh
+++ guac_install.sh
@@ -3,32 +3,34 @@
 # sh test frey frey pod1.avxlab.cc pod1 avxlab.cc
 # Arguments:  1=username 2=password 3=fqdn 4=pod_id 5=domain_name
 
+LOG="/tmp/guac-install.log"
+
 # Install all the needed packages
-sudo apt-get update >> ~/guac-install.log
-sudo apt-get install xrdp lxde make gcc g++ libcairo2-dev libjpeg-turbo8-dev libpng-dev libtool-bin libossp-uuid-dev libavcodec-dev libavutil-dev libswscale-dev freerdp2-dev libpango1.0-dev libssh2-1-dev libvncserver-dev libtelnet-dev libssl-dev libvorbis-dev libwebp-dev tomcat9 tomcat9-admin tomcat9-common tomcat9-user nginx -y >> ~/guac-install.log
+sudo apt-get update >> $LOG
+sudo apt-get install xrdp lxde make gcc g++ libcairo2-dev libjpeg-turbo8-dev libpng-dev libtool-bin libossp-uuid-dev libavcodec-dev libavutil-dev libswscale-dev freerdp2-dev libpango1.0-dev libssh2-1-dev libvncserver-dev libtelnet-dev libssl-dev libvorbis-dev libwebp-dev tomcat9 tomcat9-admin tomcat9-common tomcat9-user nginx -y >> $LOG
 
 # Start and enable Tomcat
-sudo systemctl start tomcat9 >> ~/guac-install.log
-sudo systemctl enable tomcat9 >> ~/guac-install.log
+sudo systemctl start tomcat9 >> $LOG
+sudo systemctl enable tomcat9 >> $LOG
 
 # Download and install Guacamole Server
-wget https://downloads.apache.org/guacamole/1.1.0/source/guacamole-server-1.1.0.tar.gz -P /tmp/ >> ~/guac-install.log
-tar xzf /tmp/guacamole-server-1.1.0.tar.gz -C /tmp/ >> ~/guac-install.log
+wget https://downloads.apache.org/guacamole/1.1.0/source/guacamole-server-1.1.0.tar.gz -P /tmp/ >> $LOG
+tar xzf /tmp/guacamole-server-1.1.0.tar.gz -C /tmp/ >> $LOG
 
 (
     cd /tmp/guacamole-server-1.1.0 
-    sudo ./configure --with-init-dir=/etc/init.d >> ~/guac-install.log
-    sudo make >> ~/guac-install.log
-    sudo make install >> ~/guac-install.log
-    sudo ldconfig >> ~/guac-install.log
+    sudo ./configure --with-init-dir=/etc/init.d >> $LOG
+    sudo make >> $LOG
+    sudo make install >> $LOG
+    sudo ldconfig >> $LOG
 )
 
-sudo systemctl start guacd >> ~/guac-install.log
-sudo systemctl enable guacd  >> ~/guac-install.log
+sudo systemctl start guacd >> $LOG
+sudo systemctl enable guacd  >> $LOG
 
 
 ####
-sudo mkdir /etc/guacamole >> ~/guac-install.log
+sudo mkdir /etc/guacamole >> $LOG
 
 echo "<user-mapping>
 <authorize 
@@ -65,10 +67,10 @@ echo "<user-mapping>
 </user-mapping>" | sudo tee -a /etc/guacamole/user-mapping.xml
 
 
-sudo wget https://downloads.apache.org/guacamole/1.1.0/binary/guacamole-1.1.0.war -O /etc/guacamole/guacamole.war  >> ~/guac-install.log
-sudo ln -s /etc/guacamole/guacamole.war /var/lib/tomcat9/webapps/  >> ~/guac-install.log
-sleep 10  >> ~/guac-install.log
-sudo mkdir /etc/guacamole/{extensions,lib}  >> ~/guac-install.log
+sudo wget https://downloads.apache.org/guacamole/1.1.0/binary/guacamole-1.1.0.war -O /etc/guacamole/guacamole.war  >> $LOG
+sudo ln -s /etc/guacamole/guacamole.war /var/lib/tomcat9/webapps/  >> $LOG
+sleep 10  >> $LOG
+sudo mkdir /etc/guacamole/{extensions,lib}  >> $LOG
 sudo bash -c 'echo "GUACAMOLE_HOME=/etc/guacamole" >> /etc/default/tomcat9'
 
 echo "guacd-hostname: localhost
@@ -78,19 +80,19 @@ auth-provider:    net.sourceforge.guacamole.net.basic.BasicFileAuthenticationPro
 
 sudo ln -s /etc/guacamole /usr/share/tomcat9/.guacamole
 
-sudo systemctl restart tomcat9  >> ~/guac-install.log
-sudo systemctl restart guacd  >> ~/guac-install.log
+sudo systemctl restart tomcat9  >> $LOG
+sudo systemctl restart guacd  >> $LOG
 
 #####
 # Create user for RDP session
-sudo useradd -m -s /bin/bash $1  >> ~/guac-install.log
+sudo useradd -m -s /bin/bash $1  >> $LOG
 echo "$1:$2" | sudo chpasswd
 
 # Download ssl cert for nginx
-sudo wget https://avx-build.s3.eu-central-1.amazonaws.com/san-cert.crt -O /etc/nginx/cert.crt  >> ~/guac-install.log
-sudo wget https://avx-build.s3.eu-central-1.amazonaws.com/san-cert.key -O /etc/nginx/cert.key  >> ~/guac-install.log
-sudo systemctl start nginx  >> ~/guac-install.log
-sudo systemctl enable nginx  >> ~/guac-install.log
+sudo wget https://avx-build.s3.eu-central-1.amazonaws.com/san-cert.crt -O /etc/nginx/cert.crt  >> $LOG
+sudo wget https://avx-build.s3.eu-central-1.amazonaws.com/san-cert.key -O /etc/nginx/cert.key  >> $LOG
+sudo systemctl start nginx  >> $LOG
+sudo systemctl enable nginx  >> $LOG
 
 # Create Desktop shortcuts
 sudo mkdir /home/$1/Desktop
@@ -107,8 +109,8 @@ Name=LXTerminal
 Icon=lxterminal
 URL=/usr/share/applications/lxterminal.desktop" | sudo tee -a /home/$1/Desktop/lxterminal.desktop
 
-sudo chown $1:$1 /home/$1/Desktop  >> ~/guac-install.log
-sudo chown $1:$1 /home/$1/Desktop/*  >> ~/guac-install.log
+sudo chown $1:$1 /home/$1/Desktop  >> $LOG
+sudo chown $1:$1 /home/$1/Desktop/*  >> $LOG
 
 # Nginx config - SSL redirect
 echo "server {
@@ -138,29 +140,29 @@ server {
 	}
 }" | sudo tee -a /etc/nginx/conf.d/default.conf
 
-sudo systemctl restart nginx  >> ~/guac-install.log
+sudo systemctl restart nginx  >> $LOG
 
 # Customize Guacamole login page
-sudo ls -l /var/lib/tomcat9/webapps/guacamole/  >> ~/guac-install.log
-sudo wget https://avx-build.s3.eu-central-1.amazonaws.com/logo-144.png  >> ~/guac-install.log
-sudo wget https://avx-build.s3.eu-central-1.amazonaws.com/logo-64.png  >> ~/guac-install.log
+sudo ls -l /var/lib/tomcat9/webapps/guacamole/  >> $LOG
+sudo wget https://avx-build.s3.eu-central-1.amazonaws.com/logo-144.png  >> $LOG
+sudo wget https://avx-build.s3.eu-central-1.amazonaws.com/logo-64.png  >> $LOG
 # while [ ! -d /var/lib/tomcat9/webapps/guacamole/images/ ]; do
 #   sleep 1
 # done
-sudo cp logo-144.png /var/lib/tomcat9/webapps/guacamole/images/  >> ~/guac-install.log
-sudo cp logo-64.png /var/lib/tomcat9/webapps/guacamole/images/  >> ~/guac-install.log
-sudo cp logo-144.png /var/lib/tomcat9/webapps/guacamole/images/guac-tricolor.png  >> ~/guac-install.log
-sudo sed -i "s/Apache Guacamole/Aviatrix Build - $4/g" /var/lib/tomcat9/webapps/guacamole/translations/en.json  >> ~/guac-install.log
-sudo systemctl restart tomcat9  >> ~/guac-install.log
-sudo systemctl restart guacd  >> ~/guac-install.log
+sudo cp logo-144.png /var/lib/tomcat9/webapps/guacamole/images/  >> $LOG
+sudo cp logo-64.png /var/lib/tomcat9/webapps/guacamole/images/  >> $LOG
+sudo cp logo-144.png /var/lib/tomcat9/webapps/guacamole/images/guac-tricolor.png  >> $LOG
+sudo sed -i "s/Apache Guacamole/Aviatrix Build - $4/g" /var/lib/tomcat9/webapps/guacamole/translations/en.json  >> $LOG
+sudo systemctl restart tomcat9  >> $LOG
+sudo systemctl restart guacd  >> $LOG
 
-sudo cp logo-64.png /usr/share/lxde/images/lxde-icon.png  >> ~/guac-install.log
-sudo wget https://avx-build.s3.eu-central-1.amazonaws.com/cne-student.pem -P /home/ubuntu/  >> ~/guac-install.log
-sudo chmod 400 /home/ubuntu/cne-student.pem  >> ~/guac-install.log
-sudo chown ubuntu:ubuntu /home/ubuntu/cne-student.pem  >> ~/guac-install.log
+sudo cp logo-64.png /usr/share/lxde/images/lxde-icon.png  >> $LOG
+sudo wget https://avx-build.s3.eu-central-1.amazonaws.com/cne-student.pem -P /home/ubuntu/  >> $LOG
+sudo chmod 400 /home/ubuntu/cne-student.pem  >> $LOG
+sudo chown ubuntu:ubuntu /home/ubuntu/cne-student.pem  >> $LOG
 
 # Add pod ID search domain
-sudo sed -i '$d' /etc/netplan/50-cloud-init.yaml  >> ~/guac-install.log
+sudo sed -i '$d' /etc/netplan/50-cloud-init.yaml  >> $LOG
 echo "            nameservers:
                 search: [$4.$5]" | sudo tee -a /etc/netplan/50-cloud-init.yaml
-sudo netplan apply  >> ~/guac-install.log
+sudo netplan apply  >> $LOG
